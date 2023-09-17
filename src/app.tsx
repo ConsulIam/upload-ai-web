65dace5d2fcf22cf1538ec663ac9cabaa31ad2d1
consuliam <consuliam@gmail.com>
2023-09-17 19:47:21 -0300

    First commit. Frontend complete

---
+++ src/app.tsx
@@ -0,0 +1,138 @@
+import { Github, Wand2 } from "lucide-react"
+import { Button } from "./components/ui/button";
+import { Separator } from "./components/ui/separator";
+import { Textarea } from "./components/ui/textarea";
+import { Label } from "./components/ui/label";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "./components/ui/select";
+import { Slider } from "./components/ui/slider";
+import { VideoInputForm } from "./components/video-input-form";
+import { PromptSelect } from "./components/prompt-select";
+import { useState } from "react";
+import { useCompletion } from "ai/react"
+
+export function App() {
+  const [temperature, setTemperature] = useState(0.4)
+  const [videoId, setVideoId] = useState<string | null>(null)
+
+
+  const {
+    input,
+    setInput,
+    handleInputChange,
+    handleSubmit,
+    completion,
+    isLoading,
+  } = useCompletion({
+    api: 'http://localhost:11337/ai/complete',
+    body: {
+      videoId,
+      temperature,
+    },
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+  
+  return (
+    <div className="min-h-screen flex flex-col">
+      <div className="px-6 py-3 flex items-center justify-between border-b">
+      <div>
+        <div className="text-xl font-bold">Upload AI</div>
+        <span className="block text-xs text-muted-foreground italic">
+          Transforming your videos into words
+        </span>
+      </div>
+
+      <div className="flex items-center gap-3">
+        <span className="text-sm text-muted-foreground">
+          Development with 💜 on NWL AI
+        </span>
+
+        <Separator orientation="vertical" className="h-6" />
+
+        <Button variant="outline">
+          <Github className="w-4 h-4 mr-2" />
+          Github
+        </Button>
+      </div>
+    </div>
+
+
+      <main className="flex-1 p-6 flex gap-6">
+        <div className="flex flex-col flex-1 gap-4">
+          <div className="grid grid-rows-2 gap-4 flex-1">
+            <Textarea 
+            className="resize-none p-4 leading-relaxed snap-none"
+            placeholder="Please include the text for the AI..."
+            value={input}
+            onChange={handleInputChange}
+            />
+            <Textarea 
+            className="resize-none p-4 leading-relaxed"
+            placeholder="Text generated for the AI..."
+            readOnly 
+            value={completion}
+            />
+          </div>
+
+          <p className="text-sm text-muted-foreground">
+          Remember: You can use the <code className="text-violet-400">{'{transcription}'}</code> variable in your prompt to add the transcript content of the selected video
+          </p>
+
+        </div>
+        <aside className="w-80 space-y-6 ">
+
+        <VideoInputForm onVideoUploaded={setVideoId} />
+
+        <Separator />
+
+        <form onSubmit={handleSubmit} className="space-y-6">
+          <div className="space-y-2">
+            <Label>Prompt</Label>
+            <PromptSelect onPromptSelected={setInput} />
+          </div>
+          
+          <div className="space-y-2">
+            <Label>Model</Label>
+            <Select disabled defaultValue="gpt3.5">
+              <SelectTrigger>
+                <SelectValue />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="gpt3.5">GPT 3.5-turbo 16k</SelectItem>
+              </SelectContent>
+            </Select>
+            <span className="block text-xs text-muted-foreground italic">
+              You will be able to customize this option soon.
+            </span>
+          </div>
+
+          <Separator />
+
+          <div className="space-y-4">
+            <Label>Temperature</Label>
+            <Slider 
+              min={0}
+              max={1}
+              step={0.1}
+              value={[temperature]}
+              onValueChange={value => setTemperature(value[0])}
+            />
+            <span className="block text-xs text-muted-foreground italic leading-relaxed">
+              Higher values tend to make the result more creative and more likely to make errors.
+            </span>
+          </div>
+
+          <Separator />
+
+          <Button disabled={isLoading} type="submit" className="w-full">
+            Execute
+            <Wand2 className="w-4 h-4 ml-2"/>
+          </Button>
+        </form>
+
+        </aside>
+      </main>
+    </div>
+  )
+}
